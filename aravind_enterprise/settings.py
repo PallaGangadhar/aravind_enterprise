ea3d7ea9be0f68c7335b83541c9aa1f1185de242
pallagangadhar <ganga11497@gmail.com>
2023-08-08 20:10:37 +0530

    vercel config

--- aravind_enterprise/settings.py
+++ aravind_enterprise/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2ou)t=e@upq$c)&&qnxjcakut$95o@+1vlut1&7+_k*wx6#8^m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store'
 ]
 
 MIDDLEWARE = [
